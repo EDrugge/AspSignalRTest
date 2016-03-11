0bbd1c6fe5755390c28774ea95ac1741f573e152
Emil Drugge <emil@drugge.com>
2016-03-11 16:20:37 +0100

    Added solution

---
+++ AspSignalRTest/AspSignalRTest/SignalRHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace AspSignalRTest
+{
+    public class SignalRHub : Hub
+    {
+        public void ButtonPress(int number)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<SignalRHub>();
+            context.Clients.All.buttonPressed(number);
+        }
+    }
+}
---
+++ AspSignalRTest/AspSignalRTest/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AspSignalRTest.Startup))]
+
+namespace AspSignalRTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            var hub = new SignalRHub();
+
+            Task.Run(() =>
+            {
+                var i = 0;
+                while (true)
+                {
+                    hub.ButtonPress(i++);
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+    }
+}
